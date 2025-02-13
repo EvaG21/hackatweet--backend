71d2b7180c00b9d0b4bcdba02858503e9b33b650
Eva <gacieva1@gmail.com>
2025-02-13 17:49:18 +0100

    backend login

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const User = require('./models/users')
 
 require('./models/connection');
 var indexRouter = require('./routes/index');
